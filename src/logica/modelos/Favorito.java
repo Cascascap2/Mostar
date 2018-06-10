221f4b81cd3a16e60fffa2835952cda64aebd653
Unknown <lucasgroba9@hotmail.com>
2018-06-09 23:49:19 -0300

    corregi unas lineas si queres probalo ahi
    me parece que le faltaban atributos y algunas anotaciones a favoritos

--- src/logica/modelos/Favorito.java
+++ src/logica/modelos/Favorito.java
@@ -1,26 +1,63 @@
 package logica.modelos;
 
-import javax.persistence.Column;
+
 import javax.persistence.Embeddable;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
+
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+
 
 @Embeddable
+@Entity
 public class Favorito {
-	
-	@Column(name="Contenido_id")
+	@Id
+	int id;
 	int contenido_id;
+	int usuario_id;
 	
 
 	public Favorito(){
 		
 	}
 	
+	
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public int getContenido_id() {
+		return contenido_id;
+	}
+
+
+
+	public void setContenido_id(int contenido_id) {
+		this.contenido_id = contenido_id;
+	}
+
+
+
+	public int getUsuario_id() {
+		return usuario_id;
+	}
+
+
+
+	public void setUsuario_id(int usuario_id) {
+		this.usuario_id = usuario_id;
+	}
+
+
+
 	Favorito(int contenido){
 		this.contenido_id = contenido;
 	}
