849d3c14687aae85246e25d45c50480201fe4bb2
Cascascap <cascascap2@gmail.com>
2018-06-25 04:08:49 -0300

    Estrenos

--- src/beans/homeController.java
+++ src/beans/homeController.java
@@ -6,17 +6,18 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
-import controladores.ComentarioControlador;
 import controladores.ContenidoControlador;
 import controladores.Manejador;
 import logica.modelos.Contenido;
 
 public class homeController {
 
+	private static final int ESTRENOS = 3;
+	
 	private String test;
 	private List<Contenido> contenidos;
-	private List<Contenido> ultimos_contenidos;
 	private List<Contenido> peliculas;
+	private List<Contenido> ultimas_peliculas;
 	
 	public String getTest() {
 		return test;
@@ -40,15 +41,13 @@ public class homeController {
 	}
 
 
-
-	public List<Contenido> getUltimos_contenidos() {
-		return ultimos_contenidos;
+	public List<Contenido> getUltimas_pelicula() {
+		return ultimas_peliculas;
 	}
 
 
-
-	public void setUltimos_contenidos(List<Contenido> ultimos_contenidos) {
-		this.ultimos_contenidos = ultimos_contenidos;
+	public void setUltimas_pelicula(List<Contenido> ultimas_pelicula) {
+		this.ultimas_peliculas = ultimas_pelicula;
 	}
 
 
@@ -67,14 +66,33 @@ public class homeController {
 		ContenidoControlador cc = man.getContenidoControlador();
 		this.contenidos = cc.getAllContenido();
 		this.peliculas = new ArrayList();
+		this.ultimas_peliculas = new ArrayList();
+		List<Contenido> ultimas = new ArrayList();
 		Iterator it = this.contenidos.iterator();
 		Contenido con;
+		Iterator it2 = ultimas.iterator();
 		while(it.hasNext()){
 			con = (Contenido) it.next();
-			if(con.getTipo().equals("Pelicula"))
+			if(con.getTipo().equals("Pelicula")){
 				this.peliculas.add(con);
+				if(ultimas.size()<ESTRENOS){
+					ultimas.add(con);
+				}
+				else{
+					ultimas.remove(0);
+					ultimas.add(con);
+				}
+			}
 		}
 		java.lang.System.out.println(this.contenidos.size());
+		java.lang.System.out.println("Ultimos contenidos:");
+		this.ultimas_peliculas = ultimas;
+		Iterator it3 = this.ultimas_peliculas.iterator();
+		while(it3.hasNext()){
+			Contenido con3 = (Contenido) it3.next();
+			java.lang.System.out.println(con3.getName());
+		}
+		
 	}
 	
 }
